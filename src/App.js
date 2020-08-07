44981baa8336dbc5c19a65535f8476d925ca78fa
Mask Wolf <maskwolf2000@gmail.com>
2020-08-08 03:01:37 +0530

    add upload functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React,{useState, useEffect} from 'react';
-import './App.css';
-import Post from "./Post"
-import {db, auth} from "./firebase"
-import Modal from '@material-ui/core/Modal';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import ImageUpload from "./ImageUpload"
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Post from "./Post";
+import { db, auth } from "./firebase";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import ImageUpload from "./ImageUpload";
 
 function getModalStyle() {
   const top = 50;
@@ -21,127 +21,199 @@ function getModalStyle() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
 }));
 
-
 function App() {
   const classes = useStyles();
-  const [posts, setPosts]=useState([]);
-  const [openSignIn,setOpenSignIn]=useState(false);
-  const [open, setOpen]=useState(false);
+  const [posts, setPosts] = useState([]);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [open, setOpen] = useState(false);
   const [modalStyle] = React.useState(getModalStyle);
-  const [username,setUsername]=useState('');
-  const [email,setEmail]=useState('');
-  const [password,setPassword]=useState('');
-  const [user,setUser]= useState(null);
-  useEffect(()=>{
-    const unsubscribe=auth.onAuthStateChanged((authUser)=>{
-      if(authUser){
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
         console.log(authUser);
         setUser(authUser);
-       }else {
+      } else {
         setUser(null);
       }
-    })
-    return()=>{
+    });
+    return () => {
       unsubscribe();
-    }
-  },[user,username]);
-  useEffect(()=>{
-    db.collection('posts').onSnapshot(snapshot=>{
-      setPosts(snapshot.docs.map(doc=>({
-        id:doc.id,
-        post:doc.data()
-      })))
-    })
-  },[])
-  const signIn=(e)=>{
-    e.preventDefault();
-    auth.signInWithEmailAndPassword(email,password)
-    .catch((error)=>alert(error.message));
-    setOpenSignIn(false);
-  }
-  const signup=(e)=>{
+    };
+  }, [user, username]);
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            post: doc.data(),
+          }))
+        );
+      });
+  }, []);
+  const signIn = (e) => {
     e.preventDefault();
     auth
-    .createUserWithEmailAndPassword(email,password)
-    .then((authUser)=>{
-      return authUser.user.updateProfile({
-
-        displayName:username,
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+    setOpenSignIn(false);
+  };
+  const signup = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
       })
-    }
-  )
-    .catch((error)=>alert(error.message));
-  }
+      .catch((error) => alert(error.message));
+  };
   return (
-    <div className="App">
-      {user?.displayName ?(<ImageUpload username={user.displayName}/>):""}
-    
-    <Modal
-       open={openSignIn}
-       onClose={()=>setOpenSignIn(false)}>
-
-     <div style={modalStyle} className={classes.paper}>
-
-
-     <img className="modal__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt=""/>
-     <form className="app__signup">
-
-     <div className="app__textfield"><TextField value={email} onChange={(e)=>setEmail(e.target.value)} id="outlined-basic" type="email" label="Email" variant="outlined" /></div>
-     <div className="app__textfield"><TextField value={password} onChange={(e)=>setPassword(e.target.value)} id="outlined-basic" type="password" label="Password" variant="outlined" /></div>
-
-     <div className="app__modalbutton"> <Button type="submit" onClick={signIn} variant="contained" disableElevation>Sign In</Button></div>
-    </form>
- </div>
-     </Modal>
-    <Modal
-       open={open}
-       onClose={()=>setOpen(false)}>
-
-     <div style={modalStyle} className={classes.paper}>
-
-
-     <img className="modal__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt=""/>
-     <form className="app__signup">
-     <div className="app__textfield"><TextField value={username} onChange={(e)=>setUsername(e.target.value)} id="outlined-basic" type="username" label="Username" variant="outlined" /></div>
-     <div className="app__textfield"><TextField value={email} onChange={(e)=>setEmail(e.target.value)} id="outlined-basic" type="email" label="Email" variant="outlined" /></div>
-     <div className="app__textfield"><TextField value={password} onChange={(e)=>setPassword(e.target.value)} id="outlined-basic" type="password" label="Password" variant="outlined" /></div>
-
-     <div className="app__modalbutton"> <Button type="submit" onClick={signup} variant="contained" disableElevation>Sign up</Button></div>
-    </form>
- </div>
-     </Modal>
-    <div className="app__header">
-        <img
-          className="app__headerImage"
-          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt=""/>
-
-          {user?(
-            <Button onClick={()=>auth.signOut()} color="primary" >Logout</Button>
-          ):(
-            <div className="app__loginContainer">
-              <Button onClick={()=>setOpen(true)} color="primary" >Sign up</Button>
-              <Button onClick={()=>setOpenSignIn(true)} color="primary" >LogIn</Button>
+    <div className='App'>
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <img
+            className='modal__logo'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png'
+            alt=''
+          />
+          <form className='app__signup'>
+            <div className='app__textfield'>
+              <TextField
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                id='outlined-basic'
+                type='email'
+                label='Email'
+                variant='outlined'
+              />
             </div>
-          )
-          }
-  </div>
-  <div className="app__posts">
-    {
-      posts.map(({id,post})=>(
-        <Post key={id} username={post.username} imageUrl={post.imageUrl} caption={post.caption}/>
-      ))
-    }
-    </div>
+            <div className='app__textfield'>
+              <TextField
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id='outlined-basic'
+                type='password'
+                label='Password'
+                variant='outlined'
+              />
+            </div>
+
+            <div className='app__modalbutton'>
+              {" "}
+              <Button
+                type='submit'
+                onClick={signIn}
+                variant='contained'
+                disableElevation>
+                Sign In
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <img
+            className='modal__logo'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png'
+            alt=''
+          />
+          <form className='app__signup'>
+            <div className='app__textfield'>
+              <TextField
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                id='outlined-basic'
+                type='username'
+                label='Username'
+                variant='outlined'
+              />
+            </div>
+            <div className='app__textfield'>
+              <TextField
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                id='outlined-basic'
+                type='email'
+                label='Email'
+                variant='outlined'
+              />
+            </div>
+            <div className='app__textfield'>
+              <TextField
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id='outlined-basic'
+                type='password'
+                label='Password'
+                variant='outlined'
+              />
+            </div>
+
+            <div className='app__modalbutton'>
+              {" "}
+              <Button
+                type='submit'
+                onClick={signup}
+                variant='contained'
+                disableElevation>
+                Sign up
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+      <div className='app__header'>
+        <img
+          className='app__headerImage'
+          src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+          alt=''
+        />
+
+        {user ? (
+          <Button onClick={() => auth.signOut()} color='primary'>
+            Logout
+          </Button>
+        ) : (
+          <div className='app__loginContainer'>
+            <Button onClick={() => setOpen(true)} color='primary'>
+              Sign up
+            </Button>
+            <Button onClick={() => setOpenSignIn(true)} color='primary'>
+              LogIn
+            </Button>
+          </div>
+        )}
+      </div>
+      <div className='app__posts'>
+        {posts.map(({ id, post }) => (
+          <Post
+            key={id}
+            username={post.username}
+            imageUrl={post.imageUrl}
+            caption={post.caption}
+          />
+        ))}
+      </div>
+      <footer>
+        {user?.displayName ? <ImageUpload username={user.displayName} /> : ""}
+      </footer>
     </div>
   );
 }
--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,89 +1,101 @@
-import React,{useState} from 'react'
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
-import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import {storage, db} from './firebase';
-import firebase from "firebase"   
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { storage, db } from "./firebase";
+import firebase, { firestore } from "firebase";
+import "./ImageUpload.css";
 
-function ImageUpload({username}) {
+function ImageUpload({ username }) {
   const useStyles = makeStyles((theme) => ({
     root: {
-      '& > *': {
+      "& > *": {
         margin: theme.spacing(1),
       },
     },
     input: {
-      display: 'none',
+      display: "none",
     },
   }));
-  const [caption, setCaption]=useState('');
-  const [progress,setProgress]=useState(0);
-  const [image, setImage]=useState("")
+  const [caption, setCaption] = useState("");
+  const [progress, setProgress] = useState();
+  const [image, setImage] = useState("");
   const classes = useStyles();
-  const handleChange=(e)=>{
-    if (e.target.files[0]){
-      setImage(e.target.files[0])
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
     }
   };
-  const handleUpload=()=>{
-    const uploadTask= storage.ref(`images/${image.name}`).put(image);
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
     uploadTask.on(
       "state_changed",
-      (snapshot)=>{
+      (snapshot) => {
         const progress = Math.round(
-          (snapshot.bytesTransferred/snapshot.totalBytes)*100
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
         );
         setProgress(progress);
       },
-      (error)=>{
+      (error) => {
         console.log(error);
         alert(error.message);
       },
-      ()=>{
+      () => {
         storage
           .ref("images")
           .child(image.name)
           .getDownloadURL()
-          .then(url=>{
+          .then((url) => {
             db.collection("posts").add({
-              
-              caption:caption,
-              imageUrl:url,
-              username:username,
-
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption: caption,
+              imageUrl: url,
+              username: username,
             });
             setProgress(0);
             setCaption("");
-            setImage(null)
-          })
+            setImage(null);
+          });
       }
-    )
-  }
+    );
+  };
   return (
-    <div>
-      <TextField id="standard-basic" placeholder="Enter a caption" label="Caption" onChange={event=>setCaption(event.target.value)} />
+    <div className='imageupload'>
+      <CircularProgress variant='static' value={progress} />
+      <TextField
+        id='standard-basic'
+        placeholder='Enter a caption'
+        label='Caption'
+        onChange={(event) => setCaption(event.target.value)}
+      />
       <div className={classes.root}>
-      
-      <CircularProgress variant="static" value={progress} />
-      
-        <Button onClick={handleUpload}  variant="contained" color="primary" component="span">
+        <Button
+          onClick={handleUpload}
+          variant='contained'
+          color='primary'
+          component='span'>
           Upload
         </Button>
-      <input onChange={handleChange} className={classes.input} id="icon-button-file" type="file" />
-      <label htmlFor="icon-button-file">
-        <IconButton color="primary" aria-label="upload picture" component="span">
-          <PhotoCamera />
-        </IconButton>
-      </label>
+        <input
+          onChange={handleChange}
+          className={classes.input}
+          id='icon-button-file'
+          type='file'
+        />
+        <label htmlFor='icon-button-file'>
+          <IconButton
+            color='primary'
+            aria-label='upload picture'
+            component='span'>
+            <PhotoCamera />
+          </IconButton>
+        </label>
+      </div>
     </div>
-     
-
-    </div>
-  )
+  );
 }
 
-export default ImageUpload
-
+export default ImageUpload;
